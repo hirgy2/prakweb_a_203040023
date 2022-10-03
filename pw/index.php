b2c620ed832ddb09968f6cd48f11e89a149fe40c
hirgy2 <m.rhyamizard.203040023@mail.unpas.ac.id>
2022-10-03 13:58:40 +0700

    mengubah warna background menjadi darkcyan dan mengubah text menjadi warna putih

--- pw/index.php
+++ pw/index.php
@@ -21,11 +21,12 @@ $buku = $rows;
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Buku</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 
-  <h1>Daftar Buku Komik Hehe</h1>
+  <h1>Daftar Buku Komik uhuy</h1>
 
   <table border="1" cellpading="10" cellspacing="0">
     <tr>
