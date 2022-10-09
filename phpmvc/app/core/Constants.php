2bc7d8834c9a896a7a624bbf5fc005d903860787
hirgy2 <m.rhyamizard.203040023@mail.unpas.ac.id>
2022-10-09 21:10:55 +0700

    menyelesaikan video 6 tentangAssets

---
+++ phpmvc/app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://localhost/prakweb_a_203040023/phpmvc/public');
--- phpmvc/app/views/about/index.php
+++ phpmvc/app/views/about/index.php
@@ -1,3 +1,8 @@
-    <h1>About Me</h1>
+<div class="container">
+
+<h1>About Me</h1>
+<img src="<? BASEURL; ?>/img/bergaya.jpg" alt="Hirgy" width="200" class="rounded-circle shadow">
     <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, 
     saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+
+</div>
